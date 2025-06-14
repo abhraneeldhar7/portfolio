40504f5d64ae66efee4b37dddde5e9765c8cb126
Abhraneel <abhraneeldhar7@gmail.com>
2025-06-15 01:19:24 +0530

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -197,7 +197,7 @@ export default function RotPage() {
               </div>
             </Link>
 
-            <Link href="github.com/abhraneeldhar7" target="_blank">
+            <Link href="https://github.com/abhraneeldhar7" target="_blank">
               <div className={styles.socialsItem}>
                 <Github size={15} color="white" />
                 <p>Github</p>
@@ -219,7 +219,7 @@ export default function RotPage() {
               </div>
             </Link>
 
-            <Link href="linkedin.com/in/abhraneeldhar" target="_blank">
+            <Link href="https://www.linkedin.com/in/abhraneeldhar/" target="_blank">
               <div className={styles.socialsItem}>
                 <Linkedin size={15} color="white" />
                 <p>LinkedIn</p>
