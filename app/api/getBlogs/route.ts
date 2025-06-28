1a665837febfa5f7e941f3d98dd89c7e99507166
Abhraneel <abhraneeldhar7@gmail.com>
2025-06-28 23:54:04 +0530

    added blogs url

--- app/api/getBlogs/route.ts
+++ app/api/getBlogs/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-    const res = await fetch(`${process.env.BUGSPOT_API}?userId=101766837132725568508`);
+    const res = await fetch(`https://bugspot.vercel.app/api/getBlogs?userId=101766837132725568508`);
   
     if (res.ok) {
       const data = await res.json();
