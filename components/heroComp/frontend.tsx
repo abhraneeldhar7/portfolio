2e6412976171295f948d19f59e2a5d60b54320bb
Abhraneel <abhraneeldhar7@gmail.com>
2025-10-10 23:36:32 +0530

    lil changes

--- components/heroComp/frontend.tsx
+++ components/heroComp/frontend.tsx
@@ -6,18 +6,18 @@ export default function FrontendBox() {
 
     return (
         <div className="relative">
-
+            {/* <div className={styles.shadowDiv}></div> */}
             <div className="w-[200px] h-[100px] overflow-hidden relative">
                 <div className={styles.main}>
                     <div className="relative w-full h-full">
-                        <Image src="/laptop-mockup.png" unoptimized alt="" height={200} width={200} priority/>
+                        <Image src="/laptop-mockup.png" unoptimized alt="" height={200} width={200} priority />
                     </div>
-                    <h1 className="font-[Satoshi] absolute top-[50%] left-[50%] translate-y-[-30%] translate-x-[-50%] text-[22px] font-[600]">
+                    <h1 className={`font-[Satoshi] absolute top-[40px] left-[50%] translate-x-[-50%] text-[22px] font-[600] z-[2] ${styles.heading}`}>
                         Front-end
                     </h1>
+                    <div className={styles.orb}></div>
                 </div>
             </div>
-            <div className={styles.bottomDiv}></div>
         </div>
     )
 }
