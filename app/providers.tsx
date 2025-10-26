39f09f700f17b18bfbad70cd8f8904a8c9b81276
Abhraneel <abhraneeldhar7@gmail.com>
2025-10-26 22:17:10 +0530

    some changes

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,7 @@
 import { ThemeProvider } from "next-themes"
 
 export function Providers({children}:{children:React.ReactNode}){
-    return <ThemeProvider attribute="class" defaultTheme="dark">
+    return <ThemeProvider attribute="class" defaultTheme="system">
         {children}
     </ThemeProvider>
 }
